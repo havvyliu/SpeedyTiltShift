d66d3240d90c6450e50618e2615aa42d0f8b0023
havvyliu <havvyliu@gmail.com>
2017-09-27 21:09:25 -0700

    Refined java code

--- app/src/main/java/meteor/asu/edu/speedytiltshift/SpeedyTiltShift.java
+++ app/src/main/java/meteor/asu/edu/speedytiltshift/SpeedyTiltShift.java
@@ -14,14 +14,16 @@ public class SpeedyTiltShift {
         System.loadLibrary("nativetiltshift-lib");
     }
 
+    private static float[][] GaussianKernel;
 
-//    Gaussian Function that is used to calculate the each entry of kernel function
-    public static float GaussianResult (int x, int y, float sigma) {
-        float x_f = (float) x;
-        float y_f = (float) y;
-        float G = (float) (1.00/(2.00*Math.PI*sigma*sigma) * (Math.pow(Math.E, (-1.00)*(x_f*x_f + y_f*y_f)/(2.00*sigma*sigma))));
-//        Log.d("front",String.valueOf(Math.pow(Math.E, (-1.00)*(x_f*x_f + y_f*y_f)/(2.00*sigma*sigma))));
-        return G;
+    //     Calculate the guassian matrix in advance
+    public static void CalculateGaussianKernel (int x, int y, float sigma) {
+        GaussianKernel = new float[x][y];
+        for(int i=0; i<x; i++) {
+            for(int j=0; i<y; j++) {
+                GaussianKernel[i][j] =(float) (1.00/(2.00*Math.PI*sigma*sigma) * (Math.pow(Math.E, (-1.00)*(x*x + y*y)/(2.00*sigma*sigma))));
+            }
+        }
     }
 
     public static int getPoint(int x, int y, int[] pixels, int width, int height) {
@@ -37,68 +39,14 @@ public class SpeedyTiltShift {
         int p_GG = (p_new<<8) & 0xff;
         int p_RR = (p_new<<16) & 0xff;
         int p_AA = (p_new<<24) & 0xff;
-//        for(int i = r*(-1); i<= r ; i++) {
-//            int p = getPoint(x-r, y+i, pixels, width, height);
-//            float gau = GaussianResult(i, (-1)*r, sigma);
-//            int BB = p & 0xff;
-//            int GG = (p<<8) & 0xff;
-//            int RR = (p<<16) & 0xff;
-//            int AA = (p<<24) & 0xff;
-//            p_BB += BB*gau;
-//            Log.d("p_BB",String.valueOf(BB*gau));
-//            p_GG += GG*gau;
-//            p_RR += RR*gau;
-////            Log.d("p_BB",String.valueOf(gau));
-//            p_AA += AA*gau;
-//
-////            p_new = (p_AA & 0xff) << 24 | (p_RR & 0xff) << 16 | (p_GG & 0xff) << 8 | (p_BB & 0xff);
-////            p_new +=(int) (GaussianResult(i, (-1)*r, sigma)*getPoint(x-r, y+i, pixels, width, height));
-////            p_new = p;
-//        }
-//        for(int i = r*(-1); i<= r ; i++) {
-//            int p = getPoint(x, y+i, pixels, width, height);
-//            float gau = GaussianResult(i, 0, sigma);
-//            int BB = p & 0xff;
-//            int GG = (p<<8) & 0xff;
-//            int RR = (p<<16) & 0xff;
-//            int AA = (p<<24) & 0xff;
-//            p_BB += BB*gau;
-//            p_GG += GG*gau;
-//            p_RR += RR*gau;
-//            p_AA += AA*gau;
-//
-////            p_new = (p_AA & 0xff) << 24 | (p_RR & 0xff) << 16 | (p_GG & 0xff) << 8 | (p_BB & 0xff);
-////            p_new+=(int) (GaussianResult(i, (-1)*r, sigma)*getPoint(x, y+i, pixels, width, height));
-////            p_new = p;
-//        }
-//        for(int i = r*(-1); i<= r ; i++) {
-//            int p = getPoint(x+r, y+i, pixels, width, height);
-//            float gau = GaussianResult(i, (1)*r, sigma);
-//            int BB = p & 0xff;
-//            int GG = (p<<8) & 0xff;
-//            int RR = (p<<16) & 0xff;
-//            int AA = (p<<24) & 0xff;
-//            p_BB += BB*gau;
-//            p_GG += GG*gau;
-//            p_RR += RR*gau;
-//            p_AA += AA*gau;
-//
-////            p_new = (p_AA & 0xff) << 24 | (p_RR & 0xff) << 16 | (p_GG & 0xff) << 8 | (p_BB & 0xff);
-////            p_new +=(int) (GaussianResult(i, (-1)*r, sigma)*getPoint(x+r, y+i, pixels, width, height));
-////            p_new = p;
-//        }
-//        p_new = (p_AA & 0xff) << 24 | (p_RR & 0xff) << 16 | (p_GG & 0xff) << 8 | (p_BB & 0xff);
-
-//        return p_new;
-//
         int length=pixels.length;
         int height_1=length/width;
         for( int i=r*(-1); i<=r; i++){
             if(x+i>=0 && x+i<width){
                 for( int j=r*(-1); j<=r; j++){
                     if(y+j>=0 && y+j<height_1){
-                        int p = getPoint(x+r, y+i, pixels, width, height);
-                        float gau = GaussianResult(i, j, sigma);
+                        int p = getPoint(x+i, y+j, pixels, width, height);
+                        float gau = GaussianKernel[x+i][y+j];
                         int BB = p & 0xff;
                         int GG = (p<<8) & 0xff;
                         int RR = (p<<16) & 0xff;
@@ -132,25 +80,6 @@ public class SpeedyTiltShift {
         Log.d("TILTSHIFT_JAVA","hey:"+width+","+height);
         int[] pixels = new int[width*height];
 
-        int[] BB=new int[width*height];
-        for(int i=0;i<width*height;i++){
-            BB[i]=(pixels[i]&0xff);
-        }
-
-        int[] GG=new int[width*height];
-        for(int i=0;i<width*height;i++){
-            GG[i]=((pixels[i]<<8)&0xff);
-        }
-
-        int[] RR=new int[width*height];
-        for(int i=0;i<width*height;i++){
-            RR[i]=((pixels[i]<<16)&0xff);
-        }
-
-        int[] AA=new int[width*height];
-        for(int i=0;i<width*height;i++){
-            AA[i]=((pixels[i]<<24)&0xff);
-        }
 
         int offset=0;
         int stride = width;
@@ -168,15 +97,9 @@ public class SpeedyTiltShift {
 
 //                Divide by height and apply according procedure
                 if(y <= a0) {
-//                    BB[y*width+x]=Weight_matrix(r_far, y, x, BB, width, s_far, height);
-//                    GG[y*width+x]=Weight_matrix(r_far, y, x, GG, width, s_far, height);
-//                    RR[y*width+x]=Weight_matrix(r_far, y, x, RR, width, s_far, height);
-//                    AA[y*width+x]=Weight_matrix(r_far, y, x, AA, width, s_far, height);
-//                    int color = (AA[y*width+x] & 0xff) << 24 | (RR[y*width+x] & 0xff) << 16 | (GG[y*width+x] & 0xff) << 8 | (BB[y*width+x] & 0xff);
-//                    pixels[y*width+x] = color;
-
 //                    Apply guassian blur(through weighted sum
                     if (s_far >= 0.7) {
+                        CalculateGaussianKernel(width, a0+r_far, s_far);
                         pixels[y * width + x] = Weight_matrix(r_far, y, x, pixels, width, s_far, height);
                     }
                 }
@@ -184,6 +107,7 @@ public class SpeedyTiltShift {
 ////                   Apply guassian blur(through weighted sum
                     float sigma = s_far*(a1-y)/(a1-a0);
                     if (sigma >= 0.7) {
+                        CalculateGaussianKernel(width, height, sigma); //TODO: fixme
                         pixels[y * width + x] = Weight_matrix(r_far, y, x, pixels, width, sigma, height);
                     }
                 }
@@ -193,12 +117,14 @@ public class SpeedyTiltShift {
                 else if(y <= a3) {
                     float sigma = s_far*(y-a2)/(a3-a2);
                     if(sigma >= 0.7) {
+                        CalculateGaussianKernel(width, height, sigma);
                         pixels[y * width + x] = Weight_matrix(r_near, y, x, pixels, width, sigma, height);
                     }
                 }
                 else {
 //                     Apply guassian blur(through weighted sum
                     if (s_near >= 0.7) {
+                        CalculateGaussianKernel(width, height, s_near);
                         pixels[y * width + x] = Weight_matrix(r_near, y, x, pixels, width, s_near, height);
                     }
                 }
