887ad970e12601d49d3dee3b049cb8631e07e980
havvyliu <havvyliu@gmail.com>
2017-09-28 21:13:30 -0700

    Seems to work but still blue don't know why

--- app/src/main/cpp/nativetiltshift-lib.cpp
+++ app/src/main/cpp/nativetiltshift-lib.cpp
@@ -1,6 +1,14 @@
 #include <jni.h>
 #include <string>
+#include <math.h>
 #include <arm_neon.h>
+using namespace std;
+
+JNIEXPORT jfloat JNICALL
+Java_meteor_asu_edu_speedytiltshift_SpeedyTiltShift_CalculateGaussianKernel(JNIEnv *env, jobject This, jint x, jint y, jfloat sigma) {
+    return (float) (1.00/(2.00*M_PI*sigma*sigma) * exp((-1.00)*(x*x + y*y)/(2.00*sigma*sigma)));
+}
+
 extern "C" {
 JNIEXPORT jintArray JNICALL
 Java_meteor_asu_edu_speedytiltshift_SpeedyTiltShift_nativeTiltShift(JNIEnv *env,
@@ -13,6 +21,47 @@ Java_meteor_asu_edu_speedytiltshift_SpeedyTiltShift_nativeTiltShift(JNIEnv *env,
     jint *pixels = env->GetIntArrayElements(pixels_, NULL);
     long length = env->GetArrayLength(pixels_);
     jintArray pixelsOut = env->NewIntArray(length);
+
+    jint r_far =(int) ceil(3*s_far);
+    jint r_near = (int) ceil(3*s_near);
+
+    // traverse through entire pixels array
+    for(int x=0; x<width; x++) {
+        for(int y=0; y<height; y++) {
+            if(y <= a0) {
+//                    Apply guassian blur(through weighted sum
+                if (s_far >= 0.7) {
+//                    CalculateGaussianKernel(width, a0+r_far, s_far);
+//                    pixels[y * width + x] = Weight_matrix(r_far, y, x, pixels, width, s_far, height);
+                }
+            }
+            else if(y <= a1) {
+////                   Apply guassian blur(through weighted sum
+                float sigma = s_far*(a1-y)/(a1-a0);
+                if (sigma >= 0.7) {
+//                    CalculateGaussianKernel(width, height, sigma); //TODO: fixme
+//                    pixels[y * width + x] = Weight_matrix(r_far, y, x, pixels, width, sigma, height);
+                }
+            }
+            else if(y <= a2) {
+//                    No nlur
+            }
+            else if(y <= a3) {
+                float sigma = s_far*(y-a2)/(a3-a2);
+                if(sigma >= 0.7) {
+//                    CalculateGaussianKernel(width, height, sigma);
+//                    pixels[y * width + x] = Weight_matrix(r_near, y, x, pixels, width, sigma, height);
+                }
+            }
+            else {
+//                     Apply guassian blur(through weighted sum
+                if (s_near >= 0.7) {
+//                    CalculateGaussianKernel(width, height, s_near);
+//                    pixels[y * width + x] = Weight_matrix(r_near, y, x, pixels, width, s_near, height);
+                }
+            }
+        }
+    }
 
     env->SetIntArrayRegion(pixelsOut, 0, length, pixels);
     env->ReleaseIntArrayElements(pixels_, pixels, 0);
--- app/src/main/java/meteor/asu/edu/speedytiltshift/SpeedyTiltShift.java
+++ app/src/main/java/meteor/asu/edu/speedytiltshift/SpeedyTiltShift.java
@@ -15,6 +15,7 @@ public class SpeedyTiltShift {
     }
 
     private static float[][] GaussianKernel;
+    private static float[] GaussianVector;
 
     //     Calculate the guassian matrix in advance
     public static void CalculateGaussianKernel (int x, int y, float sigma) {
@@ -23,6 +24,14 @@ public class SpeedyTiltShift {
             for(int j=0; i<y; j++) {
                 GaussianKernel[i][j] =(float) (1.00/(2.00*Math.PI*sigma*sigma) * (Math.pow(Math.E, (-1.00)*(x*x + y*y)/(2.00*sigma*sigma))));
             }
+        }
+    }
+
+    //  Calculate gaussian vector
+    public static void CalculateGaussianVector (int r, float sigma) {
+        GaussianVector = new float[r+1];
+        for(int i=0; i<=r; i++) {
+            GaussianVector[i] = (float) (1.00/Math.sqrt(2.00*Math.PI*sigma*sigma) * (Math.pow(Math.E, (-1.00)*(i*i)/(2.00*sigma*sigma))));
         }
     }
 
@@ -65,6 +74,44 @@ public class SpeedyTiltShift {
         return p_new;
     }
 
+    public static int Weight_matrix_through_vector (int r, int y, int x, int[] pixels, int width, float sigma, int height, boolean first) {
+        int p_new = 0;
+        int p_BB = p_new & 0xff;
+        int p_GG = (p_new<<8) & 0xff;
+        int p_RR = (p_new<<16) & 0xff;
+        int p_AA = (p_new<<24) & 0xff;
+        if(first) {
+            for (int i = r * (-1); i <= r; i++) {
+                float gau = GaussianVector[Math.abs(i)];
+                int p = getPoint(x, y+i, pixels, width, height);
+                int BB = p & 0xff;
+                int GG = (p << 8) & 0xff;
+                int RR = (p << 16) & 0xff;
+                int AA = (p << 24) & 0xff;
+                p_BB += BB * gau;
+                p_GG += GG * gau;
+                p_RR += RR * gau;
+                p_AA += AA * gau;
+            }
+        }
+        else {
+            for (int i = r * (-1); i <= r; i++) {
+                float gau = GaussianVector[Math.abs(i)];
+                int p = getPoint(x+i, y, pixels, width, height);
+                int BB = p & 0xff;
+                int GG = (p << 8) & 0xff;
+                int RR = (p << 16) & 0xff;
+                int AA = (p << 24) & 0xff;
+                p_BB += BB * gau;
+                p_GG += GG * gau;
+                p_RR += RR * gau;
+                p_AA += AA * gau;
+            }
+        }
+        p_new = (p_AA & 0xff) << 24 | (p_RR & 0xff) << 16 | (p_GG & 0xff) << 8 | (p_BB & 0xff);
+        return p_new;
+    }
+
     public static Bitmap tiltshift_java(Bitmap in, int a0, int a1, int a2, int a3, float s_far, float s_near){
         Bitmap out;
         out=in.copy(in.getConfig(),true);
@@ -75,60 +122,52 @@ public class SpeedyTiltShift {
         int r_far =(int) Math.ceil(3*s_far);
         int r_near = (int) Math.ceil(3*s_near);
 
-
-
-        Log.d("TILTSHIFT_JAVA","hey:"+width+","+height);
         int[] pixels = new int[width*height];
 
 
         int offset=0;
         int stride = width;
         in.getPixels(pixels,offset,stride,0,0,width,height);
-        for (int y=0; y<height; y++){
-            for (int x = 0; x<width; x++){
-//                // From Google Developer: int color = (A & 0xff) << 24 | (R & 0xff) << 16 | (G & 0xff) << 16 | (B & 0xff);
-//                int p = pixels[y*width+x];
-//                int BB = p & 0xff;
-//                int GG = (p<<8)& 0xff;
-//                int RR = 0xff;//(p<<16)& 0xff; //set red high
-//                int AA = (p<<24)& 0xff;
-//                int color = (AA & 0xff) << 24 | (RR & 0xff) << 16 | (GG & 0xff) << 8 | (BB & 0xff);
-//                pixels[y*width+x] = color;
 
-//                Divide by height and apply according procedure
-                if(y <= a0) {
-//                    Apply guassian blur(through weighted sum
-                    if (s_far >= 0.7) {
-                        CalculateGaussianKernel(width, a0+r_far, s_far);
-                        pixels[y * width + x] = Weight_matrix(r_far, y, x, pixels, width, s_far, height);
-                    }
-                }
-                else if(y <= a1) {
-////                   Apply guassian blur(through weighted sum
-                    float sigma = s_far*(a1-y)/(a1-a0);
-                    if (sigma >= 0.7) {
-                        CalculateGaussianKernel(width, height, sigma); //TODO: fixme
-                        pixels[y * width + x] = Weight_matrix(r_far, y, x, pixels, width, sigma, height);
-                    }
-                }
-                else if(y <= a2) {
-//                    No nlur
-                }
-                else if(y <= a3) {
-                    float sigma = s_far*(y-a2)/(a3-a2);
-                    if(sigma >= 0.7) {
-                        CalculateGaussianKernel(width, height, sigma);
-                        pixels[y * width + x] = Weight_matrix(r_near, y, x, pixels, width, sigma, height);
-                    }
-                }
-                else {
-//                     Apply guassian blur(through weighted sum
-                    if (s_near >= 0.7) {
-                        CalculateGaussianKernel(width, height, s_near);
-                        pixels[y * width + x] = Weight_matrix(r_near, y, x, pixels, width, s_near, height);
+        // need to do transformation twice
+        boolean first = true;
+        int counter = 0;
+
+        while(counter<=1) {
+            int[] native_pixels = pixels;
+            for (int y=0; y<height; y++) {
+                for (int x = 0; x < width; x++) {
+                    //                Divide by height and apply according procedure
+                    if (y <= a0) {
+                        if (s_far >= 0.7) {
+                            CalculateGaussianVector(r_far, s_far);
+                            pixels[y * width + x] = Weight_matrix_through_vector(r_far, y, x, pixels, width, s_far, height, first);
+                        }
+                    } else if (y <= a1) {
+                        float sigma = s_far * (a1 - y) / (a1 - a0);
+                        if (sigma >= 0.7) {
+                            CalculateGaussianVector(r_far, sigma);
+                            pixels[y * width + x] = Weight_matrix_through_vector(r_far, y, x, pixels, width, sigma, height, first);
+                        }
+                    } else if (y <= a2) {
+                        //                    No blur
+
+                    } else if (y <= a3) {
+                        float sigma = s_far * (y - a2) / (a3 - a2);
+                        if (sigma >= 0.7) {
+                            CalculateGaussianVector(r_near, sigma);
+                            pixels[y * width + x] = Weight_matrix_through_vector(r_near, y, x, pixels, width, sigma, height, first);
+                        }
+                    } else {
+                        if (s_near >= 0.7) {
+                            CalculateGaussianVector(r_near, s_near);
+                            pixels[y * width + x] = Weight_matrix_through_vector(r_near, y, x, pixels, width, s_near, height, first);
+                        }
                     }
                 }
             }
+            first = false;
+            counter++;
         }
         out.setPixels(pixels,offset,stride,0,0,width,height);
 
